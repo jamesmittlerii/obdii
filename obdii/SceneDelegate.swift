7d13b013e872958fd623a58c564713e1ab51aea4
James Mittler II <jamesmittlerii@my.smccd.edu>
2025-11-10 18:03:19 -0800

    try disabling on ipad

--- obdii/SceneDelegate.swift
+++ obdii/SceneDelegate.swift
@@ -11,5 +11,49 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = scene as? UIWindowScene else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+
+#if RELEASE
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            window.rootViewController = UnsupportedDeviceViewController()
+            self.window = window
+            window.makeKeyAndVisible()
+            return
+        }
+#endif
+
+        // Normal root for supported devices (e.g., iPhone)
+        window.rootViewController = ViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
 }
 
+/// Simple placeholder screen shown on iPad in RELEASE builds.
+/// Replace with your own implementation if desired.
+final class UnsupportedDeviceViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+        let label = UILabel()
+        label.text = "This CarPlay enabled app is not supported on iPad."
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.textColor = .label
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
