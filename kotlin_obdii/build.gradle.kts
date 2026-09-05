c51e8c80cb3765ddfa7bd756e93dfcf88accae6f
Jim Mittler <chicagozer@gmail.com>
2026-09-05 16:43:23 -0700

    update gradle

--- kotlin_obdii/build.gradle.kts
+++ kotlin_obdii/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "2.3.21" apply false
-    id("com.android.application") version "9.2.1" apply false
+    id("com.android.application") version "9.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
     id("org.jetbrains.compose") version "1.7.3" apply false
 }
